7ef65ae7556ddf62212b93b7b4790a0b48469ac1
Tobias Oberrauch <tobias.oberrauch@grolba.com>
2015-07-29 00:18:04 +0200

    Add bower support with browser testing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*global require, module*/
 /*!
- * key-validator <https://github.com/tobiasoberrauch/key-validator>
+ * missing-keys <https://github.com/tobiasoberrauch/missing-keys>
  *
  * Copyright (c) 2015, Tobias Oberrauch.
  * Licensed under the MIT License.
@@ -8,32 +8,41 @@
 
 'use strict';
 
-/**
- *
- * @param object
- * @param expectedKeys
- * @returns {Array.<T>|*}
- */
-function missingKeys(object, expectedKeys) {
-  var actualKeys = Object.keys(object);
-  return expectedKeys.filter(function (expectedKey) {
-    return actualKeys.indexOf(expectedKey) === -1;
-  });
-}
+(function (factory) {
+  if (typeof module === "object" && typeof module.exports === "object") {
+    module.exports = factory();
+  }
+  else if (typeof window === "object") {
+    window.missingKeys = factory();
+  }
+}(function () {
+  /**
+   *
+   * @param object
+   * @param expectedKeys
+   * @returns {Array.<T>|*}
+   */
+  function missingKeys(object, expectedKeys) {
+    var actualKeys = Object.keys(object);
+    return expectedKeys.filter(function (expectedKey) {
+      return actualKeys.indexOf(expectedKey) === -1;
+    });
+  }
 
-/**
- * @type {missingKeys}
- */
-missingKeys.get = missingKeys;
+  /**
+   * @type {missingKeys}
+   */
+  missingKeys.get = missingKeys;
 
-/**
- *
- * @param object
- * @param expectedKeys
- * @returns {boolean}
- */
-missingKeys.has = function (object, expectedKeys) {
-  return missingKeys(object, expectedKeys).length > 0;
-};
+  /**
+   *
+   * @param object
+   * @param expectedKeys
+   * @returns {boolean}
+   */
+  missingKeys.has = function (object, expectedKeys) {
+    return missingKeys(object, expectedKeys).length > 0;
+  };
 
-module.exports = missingKeys;
+  return missingKeys;
+}));
